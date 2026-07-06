1338664ba618ddc8e8e92449f3bdc4e81f4792e0
Fernanda <silva_gomes_fernan01@student.smc.edu>
2026-07-05 22:08:29 -0700

    Update Catmessageform.php

--- module3/Catmessageform.php
+++ module3/Catmessageform.php
@@ -4,3 +4,18 @@
     <meta charset="UTF-8">
     <title>Cat Message Form</title>
 </head>
+<body>
+ 
+<?php
+
+//Fernanda Gomes
+
+// Running list of validation error messages
+$errors = [];
+ 
+// Default field values 
+$FullName = "";
+$Email    = "";
+$Topic    = "";
+$Message  = "";
+ 
